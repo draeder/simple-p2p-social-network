9cec29671c522ea8a57047eb99cf97bbb2b626f5
draeder <draeder@gmail.com>
2020-08-03 18:05:30 -0500

    working on initial message handling

--- spsn-api/spsn-api.js
+++ spsn-api/spsn-api.js
@@ -170,11 +170,56 @@ function SPSN(config, customId) {
         }
     }
     customElements.define('spsn-qr', QRcode)
+
     SPSN.prototype.post = function(post){
         console.log(post)
+        class Post extends HTMLElement {
+            connectedCallback(){
+                this.innerHTML = 
+                `
+                    <article class="media box container">
+                        <figure class="image is-64x64 media-left">
+                            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                        </figure>
+                        <div class="media-content">
+                            <div class="content">
+                            <p>
+                                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                                <br>
+                                ${post.message}
+                            </p>
+                            </div>
+                            <nav class="level is-mobile">
+                            <div class="level-left">
+                                <a class="level-item" aria-label="reply">
+                                <span class="icon is-small">
+                                    <i class="fas fa-reply" aria-hidden="true"></i>
+                                </span>
+                                </a>
+                                <a class="level-item" aria-label="retweet">
+                                <span class="icon is-small">
+                                    <i class="fas fa-retweet" aria-hidden="true"></i>
+                                </span>
+                                </a>
+                                <a class="level-item" aria-label="like">
+                                <span class="icon is-small">
+                                    <i class="fas fa-heart" aria-hidden="true"></i>
+                                </span>
+                                </a>
+                            </div>
+                            </nav>
+                        </div>
+                    </article>        
+                
+                `
+            }
+        }
+        customElements.define('spsn-post', Post)
+        let element = document.createElement("spsn-post")
+        document.getElementById("posts").appendChild(element)
     }
     SPSN.post = SPSN.prototype.post
-    
+
     SPSN.prototype.profile = function(profile){
         console.log(profile)
     }
