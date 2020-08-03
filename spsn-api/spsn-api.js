fefae74f9446e6af7c3b0ac8a4b5dc2fd4dbe909
draeder <draeder@gmail.com>
2020-08-03 18:47:47 -0500

    updates

--- spsn-api/spsn-api.js
+++ spsn-api/spsn-api.js
@@ -231,6 +231,15 @@ function SPSN(config, customId) {
     }//test && e && b
 }
 
+function generateId (len) {
+    var arr = new Uint8Array((len || 40) / 2)
+    window.crypto.getRandomValues(arr)
+    return Array.from(arr, dec2hex).join('')
+}
+function dec2hex (dec) {
+    return ('0' + dec.toString(16)).substr(-2)
+}
+
 //// Message objects
     // Create a user profile object
     function Profile(type, identifier, first, last, email, about, avatar) {
